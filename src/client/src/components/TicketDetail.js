408a10dcd18a884c0b1da1946a044085238cfad1
Danielle <repko.dm@gmail.com>
2019-04-02 12:15:06 +0200

    calculated risk, excluded tickets of author

--- src/client/src/components/TicketDetail.js
+++ src/client/src/components/TicketDetail.js
@@ -10,24 +10,56 @@ export default function TicketDetail(props) {
 
     //const event = props.event.tickets.price
 
-    const numberOfComments = props.ticket.comments.length
-    console.log('number of comments', numberOfComments)
+    let risk = 5;
+    
+    const allPrices = props.event.tickets.map(ticket => ticket.price)
+    console.log('all prices', allPrices)
 
-    const price = props.event.tickets.map(ticket => ticket.price)
-    console.log('price', price)
-
-    const averagePrice = price.reduce((a,b) => a + b, 0) / price.length
+    const averagePrice = allPrices.reduce((a,b) => a + b, 0) / allPrices.length
     console.log('average price', averagePrice)
+
+    const ticketPrice = props.ticket.price
+    console.log('ticketprice', ticketPrice)
+
+    if(ticketPrice < averagePrice) {
+        const difference =  (ticketPrice/averagePrice) * 100
+        risk += difference
+    }
+    if (ticketPrice > averagePrice) {
+        const difference = (ticketPrice/averagePrice) * 100
+        if(difference < 10) {
+            risk -= difference
+        } else {
+            risk -= 10
+        }
+    }
 
     const postingHour = new Date(props.ticket.time).getHours() 
     console.log('postingtime', postingHour)
 
+    if (postingHour > 9  && postingHour < 17) {
+        risk -= 10
+    } else {
+        risk += 10
+    }
+
+    const numberOfComments = props.ticket.comments.length
+    console.log('number of comments', numberOfComments)
+
+    if (numberOfComments > 3) {
+        risk += 5
+    }
+
     const user = props.user
     console.log('user', user)
 
+    if (risk > 95) {
+        risk = 95
+    }
+
     return (
         <div>
-            <h1>Risk: {props.risk}%</h1>
+            <h1>Risk: {Math.floor(risk)}%</h1>
             <h2>Ticket from </h2>
             <h3>Price €{props.ticket.price}</h3>
             <h3>{props.ticket.description}</h3>
