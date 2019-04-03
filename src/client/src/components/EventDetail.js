330c755f26f92a596f102e07cb20e42316a9dde3
Danielle <repko.dm@gmail.com>
2019-04-03 22:33:13 +0200

    fixed position eventdetail page

--- src/client/src/components/EventDetail.js
+++ src/client/src/components/EventDetail.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import { Link } from "react-router-dom";
 import CreateTicketFormContainer from './CreateTicketFormContainer';
+import './EventDetail.css'
 
 class EventDetail extends React.Component{
 //export default function EventDetail(props) {
@@ -12,12 +13,20 @@ class EventDetail extends React.Component{
         }
             return (
                 <div>
+                  <div className = 'detail-flex'> 
+                    <div className = 'childdetail-flex'>
                     <h2>{this.props.event.name}</h2>
                     <p>Description:{this.props.event.description}</p>
                     <p>Start:{this.props.event.start}</p>
                     <p>End:{this.props.event.end}</p>
+                    </div>
+
+                    <div className = 'childdetail-flex'>
+                    <img id = 'eventdetail-img' src = {this.props.event.picture}/>
+                    </div>
+
+                    <div className = 'childdetail-flex'>
                     <h2>Available Tickets</h2>
-        
                     <ul>
                     {this.props.event.tickets.map(ticket => 
                         <li key = {ticket.id}>
@@ -27,7 +36,11 @@ class EventDetail extends React.Component{
                         </li>
                         )}
                     </ul>
+                    </div>
+                </div >  
+                <div id = 'post-ticketform'>
                     <CreateTicketFormContainer event = {this.props.event}/>
+                    </div>    
                 </div>
             )
   }
