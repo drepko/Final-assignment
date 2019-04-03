c4453647aa383f9541ec9371c58d34bdb112da23
Danielle <repko.dm@gmail.com>
2019-04-03 15:29:56 +0200

    added date filter on events list

--- src/client/src/components/EventList.js
+++ src/client/src/components/EventList.js
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 import './EventList.css'
 
 export default function EventList(props) {
+    const today = new Date().toISOString().split('T')[0]
+    console.log('today', today)
 
     //console.log(this.props.events)
     if (props.events === null) {
         return <div>Loading...</div>
     }
     return (
+
     <div className = 'parent-flex'>
     {/* <ul> */}
 
         {props.events.map(event =>
             // <li key={event.id}>
+                event.end > today &&
                 <div className = 'child-flex'>
                 <h3>{event.name}</h3>
                 <Link to={`/events/${event.id}`}><img class = 'event-logo'src = {event.picture}/></Link>
